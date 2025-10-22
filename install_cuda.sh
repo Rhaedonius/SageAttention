33cd35e59b0e1c24b656d0db14ca8ab134a65bbf
Rhaedonius <63673016+Rhaedonius@users.noreply.github.com>
2025-10-22 09:22:41 +0200

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -ex
 
-CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.8"
-curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_571.96_windows.exe
+CUDA_ROOT="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v13.0"
+curl --netrc-optional -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda_13.0.2_windows.exe
 ./cuda.exe -s
 rm cuda.exe
